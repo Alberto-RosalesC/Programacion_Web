a6253709ace6e74e3990ecfa77074300cd4a070d
Alberto Rosales <betorosales313@gmail.com>
2023-10-09 17:28:12 -0500

    Commit
    Tarea del gato

--- Parcial2/Gato/Gato.js
+++ Parcial2/Gato/Gato.js
@@ -1,7 +1,9 @@
 var XO="X";
 
+/*El parámetro e Dentro de esta función, 
+puedes escribir el código que deseas que se ejecute cuando ocurra el evento de clic en el elemento.*/
 document.getElementById("cont").addEventListener("click", function(e){
-    console.log(e)
+    console.log(e);
     if(document.getElementById(e.target.id).innerText==""){
         if(XO=="X")
             {
@@ -12,6 +14,7 @@ document.getElementById("cont").addEventListener("click", function(e){
                 XO="X"
                 e.target.innerHTML = '<img src="https://th.bing.com/th/id/R.d31e596df6fbde31d2e8b738eed1493b?rik=bfzmC6%2fOeyfEhA&riu=http%3a%2f%2f4.bp.blogspot.com%2f-J1Fwfxwo8YM%2fVXYPb1VLIdI%2fAAAAAAAABQ8%2f6EczC7G5opE%2fs1600%2fCirculo1.png&ehk=duAG0eg3rv3nntGGZ2Mw4SnxzE16OMZhcRJ8PYTF4j4%3d&risl=&pid=ImgRaw&r=0" alt="O" class="O">';
             }
+
     }
 })
 
@@ -19,6 +22,6 @@ document.getElementById("btnLim").addEventListener("click",function(){
     let cajas = document.getElementsByClassName("caja");
     for (const elemento of cajas){
         elemento.innerText="" ;
-        xo="0"; /*inicie en X cada que limpie*/
+        XO="X"
     }
 })
