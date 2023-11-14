770790feec2929b3bab41b9451c2795ce2263aa5
Alberto Rosales <betorosales313@gmail.com>
2023-11-14 15:10:23 -0600

    COMMIT

--- Parcial3/ServerExpress/index.js
+++ Parcial3/ServerExpress/index.js
@@ -1,12 +1,14 @@
 const express = require('express'); 
-
+//const cors = require('cors');
 const app = express();
+//app.use(cors());
 
 app.get('/',(req,res)=>{
+    console.log(req.query)
     res.json({mensaje: " Server Express respondiendo a get"});
 });
 
-app.post('/',(req,res)=>{
+app.post('/',(req,res)=>{   
     res.json({mensaje: " Server Express respondiendo post "})
 }); 
 
--- Parcial3/ServerHTTP/index.js
+++ Parcial3/ServerHTTP/index.js
@@ -1,12 +1,20 @@
-const http = require ('http');
- 
-const servidor = http.createServer((req,res)=>{
- 
-    res.write("Servidor http Node contestado a peditocion get");
+const http = require('http');
+
+const servidor = http.createServer((req, res) => {
+    //Configuración de encabezados para permitir CORS
+    
+    /*res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');*/
+
+    // Respuesta del servidorr
+    res.write("Servidor http Node contestado a petición GET");
     res.end();
 });
 
-servidor.listen(8082,()=>{
-    console.log("Servidor Node Http corriendo en puerto 8082")
+servidor.listen(8082, () => {
+    console.log("Servidor Node HTTP corriendo en el puerto 8082");
 });
+
 
