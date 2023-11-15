cf721b4b97261f2798b45297848eddacd1e6a59b
Alberto Rosales <betorosales313@gmail.com>
2023-11-15 15:16:49 -0600

    Commit
    Correcciones

---
+++ Parcial3/ServerExpress BD/index.js
@@ -0,0 +1,52 @@
+const express = require('express'); 
+const cors = require('cors');
+const mysql= require('mysql2');
+
+const app = express();
+app.use(cors());
+http://localhost:8082/ARTISTA
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'guero99a',
+    database: 'spotify'
+  });
+
+app.get('/ARTISTA',(req,res)=>{
+
+    console.log(req.query.ID_ARTISTA);
+
+    let consulta=''
+
+    if(typeof(req.query.ID_ARTISTA)=='undefined'){
+        consulta = `SELECT * FROM ARTISTA`;
+    }else{
+        consulta = `SELECT * FROM ARTISTA WHERE ID_ARTISTA = ${req.query.ID_ARTISTA}`;
+    }
+
+    console.log(consulta)
+
+    connection.query(
+        consulta,
+        function(err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+
+            res.json(results)
+        }
+    )
+    
+});
+
+app.post('/',(req,res)=>{   
+    res.json({mensaje: " Server Express respondiendo post "})
+}); 
+
+app.delete('/',(req,res)=>{
+    res.json({mensaje: " Server Express respondiendo a delete "})
+})
+
+app.listen(8082,(req,res)=>{
+    console.log("Servidor express corriendo en puerto 8082")
+})
+
--- Parcial3/ServerExpress/index.js
+++ Parcial3/ServerExpress/index.js
@@ -1,11 +1,11 @@
 const express = require('express'); 
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 app.get('/',(req,res)=>{
     console.log(req.query)
-    res.json({mensaje: " Server Express respondiendo a get"});
+    res.json({mensaje: " Server Express respondiendo a get "});
 });
 
 app.post('/',(req,res)=>{   
