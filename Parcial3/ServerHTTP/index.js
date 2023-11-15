f9fca3624862d1407bd5250fa654268f7b1889e0
Alberto Rosales <betorosales313@gmail.com>
2023-11-15 07:26:24 -0600

    Commit
    cosas inecesarias

--- Parcial3/ServerHTTP/index.js
+++ Parcial3/ServerHTTP/index.js
@@ -3,11 +3,8 @@ const http = require('http');
 const servidor = http.createServer((req, res) => {
     //Configuración de encabezados para permitir CORS
     
-    /*res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');*/
-
+    //res.setHeader('Access-Control-Allow-Origin', '*');
+    
     // Respuesta del servidorr
     res.write("Servidor http Node contestado a petición GET");
     res.end();
