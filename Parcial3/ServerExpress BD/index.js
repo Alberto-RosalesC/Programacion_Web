73cf3c32f9131ba969a5cc4b3a866ebc70c9ee07
Alberto Rosales <betorosales313@gmail.com>
2023-11-17 20:24:02 -0600

    commit
    Server express BD

--- Parcial3/ServerExpress BD/index.js
+++ Parcial3/ServerExpress BD/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mysql= require('mysql2');
 
-const app = express();
+const app = express(); 
 app.use(cors());
-http://localhost:8082/ARTISTA
+//http://localhost:8082/ARTISTA
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'g***a',
+    password: 'guero99a',
     database: 'spotify'
   });
 
@@ -23,16 +23,20 @@ app.get('/ARTISTA',(req,res)=>{
     }else{
         consulta = `SELECT * FROM ARTISTA WHERE ID_ARTISTA = ${req.query.ID_ARTISTA}`;
     }
+    
 
     console.log(consulta)
 
     connection.query(
         consulta,
         function(err, results, fields) {
-            console.log(results); // results contains rows returned by server
-            console.log(fields); // fields contains extra meta data about results, if available
-
-            res.json(results)
+            if(results.length==0){
+                res.json({ mensaje:"ID_PERSONA no existe"});
+            } 
+            else {
+                res.json(results);
+            }
+            
         }
     )
     
