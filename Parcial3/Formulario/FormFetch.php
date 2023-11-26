96039911c428ff15038257886678cb15181612ef
Alberto Rosales <betorosales313@gmail.com>
2023-11-26 01:11:27 -0600

    commit
    Tareas hechas

--- Parcial3/Formulario/FormFetch.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nombre=$_GET["nombre"];
-$correo=$_GET["correo"];
-$mensaje=$_GET["mensaje"];
-
-echo "Tu nombre es: "  .$nombre. " su correo es: "  .$correo. " con un mensaje que menciona: " .$mensaje;
-
-?>
