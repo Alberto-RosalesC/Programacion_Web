9c8c1117fae7adaed92ccb796abc5ecc5f4046da
Alberto Rosales <betorosales313@gmail.com>
2023-12-01 17:00:02 -0600

    commit proyecto

--- Parcial3/ServerExpress BD/index.js
+++ Parcial3/ServerExpress BD/index.js
@@ -12,45 +12,82 @@ const connection = mysql.createConnection({
     database: 'spotify'
   });
 
-app.get('/ARTISTA',(req,res)=>{
+  
+  //CONSULTA
+    app.get('/ARTISTA',(req,res)=>{
 
-    console.log(req.query.ID_ARTISTA);
+        console.log(req.query.ID_ARTISTA);
 
-    let consulta=''
+        let consulta=''
 
-    if(typeof(req.query.ID_ARTISTA)=='undefined'){
-        consulta = `SELECT * FROM ARTISTA`;
-    }else{
-        consulta = `SELECT * FROM ARTISTA WHERE ID_ARTISTA = ${req.query.ID_ARTISTA}`;
-    }
-    
-
-    console.log(consulta)
-
-    connection.query(
-        consulta,
-        function(err, results, fields) {
-            if(results.length==0){
-                res.json({ mensaje:"ID_PERSONA no existe"});
-            } 
-            else {
-                res.json(results);
-            }
-            
+        if(typeof(req.query.ID_ARTISTA)=='undefined'){
+            consulta = `SELECT * FROM ARTISTA`;
+        }else{
+            consulta = `SELECT * FROM ARTISTA WHERE ID_ARTISTA = ${req.query.ID_ARTISTA}`;
         }
-    )
-    
-});
+        
+        console.log(consulta)
 
-app.post('/',(req,res)=>{   
-    res.json({mensaje: " Server Express respondiendo post "})
-}); 
+        connection.query(
+            consulta,
+            function(err, results, fields) {
+                if(results.length==0){
+                    res.json({ status:0,
+                        mensaje:"ID no existe",
+                        datos: {} });
+                } 
+                else {
+                    res.json({status: 1,
+                            mensaje : "Artista encontrado",
+                            datos: results[0] });
+                }
+            }
+        )
+        
+    });
 
-app.delete('/',(req,res)=>{
-    res.json({mensaje: " Server Express respondiendo a delete "})
-})
+    app.post('/ARTISTA',(req,res)=>{   
+    }); 
 
-app.listen(8082,(req,res)=>{
-    console.log("Servidor express corriendo en puerto 8082")
-})
+    //ELIMINACION
+    app.delete('/ARTISTA',(req,res)=>{
+        console.log(req.query.ID_ARTISTA);
+        let sentenciaSQL = ''
+        if (typeof (req.query.ID_ARTISTA) == 'undefined') {
+            res.json({
+                status: 0,
+                mensaje: "Ingresa el ID deL ARTISTA que deseas eliminar por favor",
+                datos: {}
+            });
+        }
+        else {
+            sentenciaSQL = `DELETE FROM ARTISTA WHERE ID_ARTISTA = ${req.query.ID_ARTISTA}`;
+        }
+        console.log(sentenciaSQL);
+        connection.query(
+            sentenciaSQL,
+            function (err, results, fields) {
+                console.log(results);
+                if (results.affectedRows == 1) {
+                    res.json({
+                        status: 1,  
+                        mensaje: "ARTISTA eliminada exitosamente",
+                        datos: {}
+                    });
+                }
+                else {
+                    res.json({
+                        status: 0,
+                        mensaje: "Este ID no esta registrado, intente con otro ID por favor",
+                        datos: {}
+                    });
+                }
+            }
+        )
+
+    })
+
+    app.listen(8082,(req,res)=>{
+        console.log("Servidor express corriendo en puerto 8082")
+    })
 
