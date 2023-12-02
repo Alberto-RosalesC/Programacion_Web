473ebebeb04e2c010c983cb929aed7cde352182a
Alberto Rosales <betorosales313@gmail.com>
2023-12-01 19:43:33 -0600

    commit
    Pruebas

--- Parcial3/ServerExpress BD/index.js
+++ Parcial3/ServerExpress BD/index.js
@@ -46,8 +46,42 @@ const connection = mysql.createConnection({
         
     });
 
-    app.post('/ARTISTA',(req,res)=>{   
-    }); 
+    //AGREGAR
+    app.post('/ARTISTA', (req, res) => {
+        console.log(req.query);
+        let sentenciaSQL = '';
+        if (typeof (req.query.NOMBRE) == 'undefined' || typeof (req.query.APELLIDO) == 'undefined' || typeof (req.query.FECHA_NACIMIENTO) == 'undefined' || typeof (req.query.ACERCA_DE) == 'undefined' || typeof (req.query.PAIS) == 'undefined') {
+            res.json({
+                status: 0,
+                mensaje: "Completa todos los campos por favor",
+                datos: {}
+            });
+        }
+        else {
+            sentenciaSQL = `INSERT INTO ARTISTA (NOMBRE,APELLIDO, FECHA_NACIMIENTO, ACERCA_DE, PAIS) VALUES ('${req.query.NOMBRE}', '${req.query.APELLIDO}', '${req.query.FECHA_NACIMIENTO}', '${req.query.ACERCA_DE}', '${req.query.PAIS}')`;
+            console.log(sentenciaSQL);
+            connection.query(
+                sentenciaSQL,
+                function (err, results, fields) {
+                    console.log(results);
+                    if (results && results.affectedRows == 1) {
+                        res.json({
+                            status: 1,
+                            mensaje: "ARTISTA agregada exitosamente",
+                            datos: {}
+                        });
+                    } else {
+                        res.json({
+                            status: 0,
+                            mensaje: "Hubo un error al agregar EL ARTISTA, por favor intenta de nuevo",
+                            datos: {}
+                        });
+                    }
+                }
+            )
+        }
+    });
+    
 
     //ELIMINACION
     app.delete('/ARTISTA',(req,res)=>{
@@ -87,6 +121,40 @@ const connection = mysql.createConnection({
 
     })
 
+    app.put('/ARTISTA', (req, res) => {
+        console.log(req.query);
+        let sentenciaSQL = '';
+        if (typeof (req.query.ID_ARTISTA) == 'undefined' || typeof (req.query.NOMBRE) == 'undefined' || typeof (req.query.APELLIDO) == 'undefined' || typeof (req.query.FECHA_NACIMIENTO) == 'undefined' || typeof (req.query.ACERCA_DE) == 'undefined' || typeof (req.query.PAIS) == 'undefined') {
+            res.json({
+                status: 0,
+                mensaje: "Rellene todos los campos por favor",
+                datos: {}
+            });
+        }
+        else {
+            sentenciaSQL = `UPDATE ARTISTA SET NOMBRE = '${req.query.NOMBRE}', APELLIDO = '${req.query.APELLIDO}', FECHA_NACIMIENTO = '${req.query.FECHA_NACIMIENTO}', ACERCA_DE = '${req.query.ACERCA_DE}', PAIS = '${req.query.PAIS}' WHERE ID_ARTISTA = ${req.query.ID_ARTISTA}`;
+            console.log(sentenciaSQL);
+            connection.query(
+                sentenciaSQL,
+                function (err, results, fields) {
+                    console.log(results);
+                    if (results && results.affectedRows == 1) {
+                        res.json({
+                            status: 1,
+                            mensaje: "ARTISTA modificado exitosamente",
+                            datos: {}
+                        });
+                    } else {
+                        res.json({
+                            status: 0,
+                            mensaje: "Hubo un error al modificar el ARTISTA, por favor intenta de nuevo",
+                            datos: {}
+                        });
+                    }
+                }
+            )
+        }
+    });
     app.listen(8082,(req,res)=>{
         console.log("Servidor express corriendo en puerto 8082")
     })
